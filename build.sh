9dd8bec922fc0fe68ec4514e84d28e1d9aef0460
Dave Corley <smokydave@icloud.com>
2024-03-09 13:48:44 -0600

    Remove lateng disable script

--- build.sh
+++ build.sh
@@ -141,6 +141,9 @@ if [ "$1" = "tsi" ]; then
     tes3cmd delete --type CELL --exact-id "Taris, Upper City Cantina" --instance-match "SW_TarisHuttRagax" StarwindRemasteredPatch.esm
     tes3cmd delete --type CELL --exact-id "Nar Shaddaa, Hutt Cartel" --instance-match "SW_HuttBadhiya" StarwindRemasteredPatch.esm
 
+    # Remove lateng disable script
+    tes3cmd delete --type SCPT --exact-id "SW_CourteCompScript" StarwindRemasteredPatch.esm StarwindRemasteredV1.15.esm
+    tes3cmd modify --type NPC_ --exact-id "sw_czerkacourte22"  --replace "/SW_CourteCompScript//" StarwindRemasteredPatch.esm
 fi
 # Just cleaning
 #This script is actually SW_traveltokashyyk but I figure if anybody was actually using this script the typo would have been noticed a long time ago
