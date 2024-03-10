ddbd08482c5b648dbe1d469cc730ef8756c7d0ea
Dave Corley <smokydave@icloud.com>
2024-03-10 16:14:35 -0500

    Fix syntax errors

--- service/tes3mp-init.sh
+++ service/tes3mp-init.sh
@@ -8,8 +8,8 @@ mkdir -p data/custom/Starwind data/custom/cell
 
 curl -L 'https://gitlab.com/magicaldave1/Starwind-Builder/-/jobs/artifacts/master/download?job=dump_requiredDataFiles' | bsdtar -xvf- -C data
 
-curl -L 'https://gitlab.com/magicaldave1/Starwind-Builder/-/jobs/artifacts/master/download?job=dump_cells' | bsdtar -xvf- -O | tar -xv -C .
+curl -L 'https://gitlab.com/magicaldave1/Starwind-Builder/-/jobs/artifacts/master/download?job=dump_cells' | bsdtar -xvf- -O | tar -xz -C .
 
 curl -L 'https://gitlab.com/magicaldave1/Starwind-Builder/-/jobs/artifacts/master/download?job=dump_merchants' | bsdtar -xvf- -C data/custom
 
-./tes3mp-server
+../tes3mp-server
