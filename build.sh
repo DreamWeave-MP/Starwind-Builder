e5e85b96833f2bb94813f98b34eb90bef3762d71
Dave Corley <smokydave@icloud.com>
2024-03-10 00:32:28 -0600

    Nar shaddaa on the brain

--- build.sh
+++ build.sh
@@ -113,8 +113,8 @@ if [ "$1" = "tsi" ]; then
     tes3cmd delete --type CELL --exact-id "The Outer Rim, Freighter" --instance-match "ObjIdx:16400 " StarwindRemasteredPatch.esm StarwindRemasteredV1.15.esm # Mods Droid platform (replaced by MP plugin)
 
     # Embassy doors were replaced with more lenient scripts
-    tes3cmd delete --type CELL --exact-id "Nar Shaddaa, Republic Embassy" --instance-match "ObjIdx:6771 " StarwindRemasteredV1.15.esm
-    tes3cmd delete --type CELL --exact-id "Nar Shaddaa, Sith Embassy" --instance-match "ObjIdx:9107 " StarwindRemasteredV1.15.esm
+    tes3cmd delete --type CELL --exact-id "Manaan, Republic Embassy" --instance-match "ObjIdx:6771 " StarwindRemasteredV1.15.esm
+    tes3cmd delete --type CELL --exact-id "Manaan, Sith Embassy" --instance-match "ObjIdx:9107 " StarwindRemasteredV1.15.esm
 
     # We laid out the cell ourselves
     tes3cmd delete --type CELL --exact-id "Nar Shaddaa, Makacheesa Market" StarwindVvardenfell.esp
