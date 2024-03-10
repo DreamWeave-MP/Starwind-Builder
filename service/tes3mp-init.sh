ec16d3431f88f44296e349d738cafe5931e5b1fc
Dave Corley <smokydave@icloud.com>
2024-03-10 16:07:32 -0500

    Cleaner init script, NO TEMP FILES

--- service/tes3mp-init.sh
+++ service/tes3mp-init.sh
@@ -2,17 +2,14 @@
 
 set -eu
 
-cd /home/corleycomputerrepair/TES3MP-server/
-mkdir -p server/data/custom/Starwind server/data/custom/cell
+cd /home/corleycomputerrepair/TES3MP-server/server
 
-curl -L 'https://gitlab.com/magicaldave1/Starwind-Builder/-/jobs/artifacts/master/download?job=dump_requiredDataFiles' -o rdf.zip \
-    && unzip -o rdf.zip -d server/data
-curl -L 'https://gitlab.com/magicaldave1/Starwind-Builder/-/jobs/artifacts/master/download?job=dump_cells' -o cellDB.zip \
-    && unzip -o cellDB.zip \
-    && unzip StarwindDB.zip -d server/
-curl -L 'https://gitlab.com/magicaldave1/Starwind-Builder/-/jobs/artifacts/master/download?job=dump_merchants' -o merchants.zip \
-    && unzip -o merchants.zip -d server/data/custom
+mkdir -p data/custom/Starwind data/custom/cell
 
-rm merchants.zip cellDB.zip StarwindDB.zip rdf.zip
+curl -L 'https://gitlab.com/magicaldave1/Starwind-Builder/-/jobs/artifacts/master/download?job=dump_requiredDataFiles' | bsdtar -xvf- -C data
+
+curl -L 'https://gitlab.com/magicaldave1/Starwind-Builder/-/jobs/artifacts/master/download?job=dump_cells' | bsdtar -xvf- -O | tar -xv -C .
+
+curl -L 'https://gitlab.com/magicaldave1/Starwind-Builder/-/jobs/artifacts/master/download?job=dump_merchants' | bsdtar -xvf- -C data/custom
 
 ./tes3mp-server
