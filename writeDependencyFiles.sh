7cad5c6b2545d33b6b3db62c1a48087ff82d7a61
Dave Corley <smokydave@icloud.com>
2024-12-29 16:29:18 -0700

    FIX: I don't think this echo interprets -e but it do be workin anyway

--- writeDependencyFiles.sh
+++ writeDependencyFiles.sh
@@ -7,6 +7,6 @@ DIR="${2:-.}"
 
 mkdir -p $HOME/.local/share/openmw/data
 
-echo -e "data=\"$(pwd)\"\ncontent=Morrowind.esm\ncontent=Tribunal.esm\ncontent=Bloodmoon.esm\ncontent=Starwind.omwaddon" >> $HOME/.config/openmw/openmw.cfg
+echo "data=\"$(pwd)\"\ncontent=Morrowind.esm\ncontent=Tribunal.esm\ncontent=Bloodmoon.esm\ncontent=Starwind.omwaddon" >> $HOME/.config/openmw/openmw.cfg
 
 printf ${KEY} | gpg --batch --passphrase-fd 0 --decrypt /plugins/DATA.tar.gz.gpg | tar xzvf - -C ${DIR}
