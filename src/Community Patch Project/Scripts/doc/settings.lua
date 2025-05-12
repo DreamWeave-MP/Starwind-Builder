239e59f58d514883cfa451d11505cabcec84fdd5
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-05-12 04:08:03 -0700

    CLEANUP: All input binding types can be disabled

--- src/Community Patch Project/Scripts/doc/settings.lua
+++ src/Community Patch Project/Scripts/doc/settings.lua
@@ -13,29 +13,25 @@
 ---@class InputBindingOptions
 ---@field key string The (table) key of the binding
 ---@field type InputBindingType The type of binding
+---@field disabled boolean? Disables changing the setting from the UI. Defaults to false.
 
 ---@class ColorSettingOptions
----@field disabled boolean? Disables changing the setting from the UI. Defaults to false.
 
 ---@class SelectSettingOptions
----@field disabled boolean? Disables changing the setting from the UI. Defaults to false.
 ---@field l10n string localization context with display values for items. Required.
 ---@field items string[] list of available setting options. Defaults to empty array.
 
 ---@class NumberSettingOptions
----@field disabled boolean? Disables changing the setting from the UI. Defaults to false.
 ---@field integer boolean? Whether to only allow integer values. Defaults to false.
 ---@field min number? If set, restricts setting values below this number. Defaults to nil.
 ---@field max number? If set, restricts setting values above this number. Defaults to nil.
 
 ---@class CheckboxSettingOptions
----@field disabled boolean? Disables changing the setting from the UI. Defaults to false.
 ---@field l10n string localization context with display values for items. Defaults to 'Interface'
 ---@field trueLabel string? Localization key to display for the true value. Defaults to 'Yes'
 ---@field falseLabel string? Localization key to display for the false value. Defaults to 'No'
 
 ---@class TextLineOptions
----@field disabled boolean? Disables changing the setting from the UI. Defaults to false.
 
 ---@alias SettingRendererOptions
 ---| ColorSettingOptions
