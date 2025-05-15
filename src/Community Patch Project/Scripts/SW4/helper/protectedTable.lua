e153c8c82b53bcdd1ce77cb8ea70ee425b16a972
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-05-15 00:04:31 -0700

    CLEANUP: General improvements, formatting, and add a state table which can store arbitrary values

--- src/Community Patch Project/Scripts/SW4/helper/protectedTable.lua
+++ src/Community Patch Project/Scripts/SW4/helper/protectedTable.lua
@@ -2,15 +2,17 @@ local gameSelf = require('openmw.self')
 local storage = require('openmw.storage')
 local types = require('openmw.types')
 
-local function pairsByKeys (t, f)
+local function pairsByKeys(t, f)
   local a = {}
   for n in pairs(t) do table.insert(a, n) end
   table.sort(a, f)
   local i = 0
-  local iter = function ()
+  local iter = function()
     i = i + 1
-    if a[i] == nil then return nil
-    else return a[i], t[a[i]]
+    if a[i] == nil then
+      return nil
+    else
+      return a[i], t[a[i]]
     end
   end
   return iter
@@ -24,22 +26,30 @@ end
 ---@param modInfo ModInfo
 ---@return ProtectedTable
 return function(inputGroupName, modInfo)
-  assert(inputGroupName ~= nil and storage.globalSection(inputGroupName) ~= nil,
-         'An invalid setting group was provided!')
+  local requestedGroup = storage.globalSection(inputGroupName)
+
+  assert(inputGroupName ~= nil and requestedGroup ~= nil,
+    'An invalid setting group was provided!')
 
   local proxy = {
-    thisGroup = storage.globalSection(inputGroupName)
+    thisGroup = requestedGroup,
   }
+
+  local state = {}
   local methods = {}
 
   function proxy.debugLog(...)
     if gameSelf.type ~= types.Player or not proxy.DebugLog then return end
-    print(modInfo.logPrefix, table.concat({...}, ' '))
+    print(modInfo.logPrefix, table.concat({ ... }, ' '))
   end
 
   function proxy.notifyPlayer(...)
     if gameSelf.type ~= types.Player or not proxy.MessageEnable then return end
-    require('openmw.ui').showMessage(modInfo.logPrefix .. ' ' .. table.concat({...}, ' '))
+    require('openmw.ui').showMessage(modInfo.logPrefix .. ' ' .. table.concat({ ... }, ' '))
+  end
+
+  function proxy.getState()
+    return state
   end
 
   local meta = {
@@ -51,18 +61,24 @@ return function(inputGroupName, modInfo)
         return storage.globalSection('SettingsGlobal' .. modInfo.name):get('MessageEnable') == true
       elseif key == 'debugLog' then
         return proxy.debugLog
+      elseif key == 'state' then
+        return state
       end
       return methods[key] or proxy.thisGroup:get(key)
     end,
     __newindex = function(_, key, value)
-      if type(value) ~= 'function' then
+      if key == 'state' and type(value) == 'table' then
+        state = value
+      elseif type(value) ~= 'function' or
+          (type(value) ~= 'table' and key == 'state') then
         error(
           string.format([[%s Unauthorized table access when updating '%s' to '%s'.
 This table is not writable and values must be updated through its associated storage group: '%s'.]], modInfo.logPrefix,
-          tostring(key), tostring(value), inputGroupName),
+            tostring(key), tostring(value), inputGroupName),
           2)
+      else
+        rawset(methods, key, value)
       end
-      rawset(methods, key, value)
     end,
     __tostring = function(_)
       local members = {}
@@ -77,7 +93,7 @@ This table is not writable and values must be updated through its associated sto
       end
 
       return string.format('CHIMManager{ Members: %s Methods: %s }',
-                           table.concat(members, ', '), table.concat(methodParts, ', '))
+        table.concat(members, ', '), table.concat(methodParts, ', '))
     end,
     __pairs = function()
       return next, proxy.thisGroup:asTable(), nil
