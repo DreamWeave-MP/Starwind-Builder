6c4045513ff37517dd8f32dea28a40dd64bb35bf
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-05-15 16:49:03 -0700

    FIX: Remap returns a number too lol

--- src/Community Patch Project/Scripts/doc/util.lua
+++ src/Community Patch Project/Scripts/doc/util.lua
@@ -4,7 +4,7 @@ local util = {}
 ---@class openmw.util
 ---@field vector2 util.vector2
 ---@field vector3 util.vector3
----@field remap fun(value: number, oldMin: number, oldMax: number, newMin: number, newMax: number) remap a value from one range into another
+---@field remap fun(value: number, oldMin: number, oldMax: number, newMin: number, newMax: number): number remap a value from one range into another
 ---@field round fun(value: number): number rounds a number to the nearest whole integer
 
 return util
