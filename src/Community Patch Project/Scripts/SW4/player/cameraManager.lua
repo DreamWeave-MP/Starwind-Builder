9a793b182f57c9803aab7c260cd2cb513ad0e72e
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-05-17 19:48:11 -0700

    CLEANUP: Integrate CameraManager with the GlobalManagement pattern, strip functions now handled by inputController

--- src/Community Patch Project/Scripts/SW4/player/cameraManager.lua
+++ src/Community Patch Project/Scripts/SW4/player/cameraManager.lua
@@ -1,9 +1,11 @@
 local camera = require 'openmw.camera'
 local gameSelf = require 'openmw.self'
-local input = require 'openmw.input'
 local util = require 'openmw.util'
 
 local ModInfo = require 'Scripts.SW4.modinfo'
+
+---@type ManagementStore
+local GlobalManagement
 
 ---@class CameraManager
 local CameraManager = require 'Scripts.SW4.helper.protectedTable' (
@@ -17,7 +19,6 @@ local CameraManager = require 'Scripts.SW4.helper.protectedTable' (
 ---@field pitchDelta number yaw change between current and last frame
 ---@field pitchThisFrame number
 ---@field pitchLastFrame number
----@field isLockedOn boolean
 ---@field isWielding boolean whether or not the player has a weapon or spell drawn
 CameraManager.state = {
     yawDelta = 0,
@@ -28,19 +29,18 @@ CameraManager.state = {
     pitchThisFrame = 0,
     pitchLastFrame = 0,
 
-    isLockedOn = false,
     isWielding = false,
     canDoLockOn = false,
 }
-
--- function CameraManager.getself.state.)
---     return state
--- end
 
 function CameraManager:updateTransform()
     self.state.yawThisFrame = camera.getYaw()
     self.state.pitchThisFrame = camera.getPitch()
     self.state.isWielding = gameSelf.type.getStance(gameSelf) ~= gameSelf.type.STANCE.Nothing
+end
+
+function CameraManager:isWielding()
+    return self.state.isWielding
 end
 
 function CameraManager.isMoving()
@@ -51,23 +51,16 @@ function CameraManager.isThirdPerson()
     return camera.getMode() ~= camera.MODE.FirstPerson
 end
 
-function CameraManager:setLockedOn(state)
-    self.state.isLockedOn = state
-end
-
 --- Override yawChange from mouseInput
 ---@param dt number deltaTime
----@param managers table<string, any> Direct access to all SW4 subsystems
-function CameraManager:onFrameBegin(dt, managers)
+function CameraManager:onFrameBegin(dt)
     CameraManager:updateTransform()
 
-    --- Override yawChange from mouse inputs
-    if CameraManager.isMoving() then
-        gameSelf.controls.yawChange = 0
+    if camera.getMode() == camera.MODE.Preview then
+        camera.setMode(camera.MODE.ThirdPerson)
     end
 
-    -- Override first-person pitch change inputs (should really only be while locked on but whatever)
-    if self.state.isLockedOn then
+    if GlobalManagement.LockOn.getMarkerVisibility() then
         gameSelf.controls.pitchChange = 0
     end
 end
@@ -103,13 +96,6 @@ function CameraManager:trackTargetUsingViewport(targetObject, normalizedPos)
 
     local pitchDifference = math.asin(currentWorldDir.z) - math.asin(desiredWorldDir.z)
 
-    -- Normalize yawDifference to the range [-pi, pi]
-    if yawDifference > math.pi then
-        yawDifference = yawDifference - 2 * math.pi
-    elseif yawDifference < -math.pi then
-        yawDifference = yawDifference + 2 * math.pi
-    end
-
     camera.setYaw(util.normalizeAngle(camera.getYaw() + yawDifference))
     camera.setPitch(util.normalizeAngle(camera.getPitch() - pitchDifference))
 
@@ -120,48 +106,23 @@ function CameraManager:trackTargetUsingViewport(targetObject, normalizedPos)
 end
 
 ---@param dt number deltaTime
----@param managers table<string, any> Direct access to all SW4 subsystems
-function CameraManager:onFrameEnd(dt, managers)
+function CameraManager:onFrameEnd(dt)
     CameraManager:updateDelta()
-
-    local sideMovement = gameSelf.controls.sideMovement
-    local forwardMovement = gameSelf.controls.movement
-    local isPressingRun = input.getBooleanActionValue('Run')
-    local targetObject = managers.LockOn.getTargetObject()
-
-    -- If not currently locked or not able to lock, and moving horizontally, turn on behalf of the player
-    if not isPressingRun and (not self.state.canDoLockOn or not targetObject) and sideMovement ~= 0 then
-        gameSelf.controls.yawChange = math.rad(sideMovement * 180 * dt)
-    end
-
-    -- Only turn when not moving forward or backward, so that you don't actually strafe around.
-    if not isPressingRun and not self.state.isWielding and (sideMovement ~= 0 and forwardMovement == 0) then
-        gameSelf.controls.sideMovement = 0
-        camera.setYaw(camera.getYaw() + gameSelf.controls.yawChange)
-    end
-
-    -- override the behavior of the `Run` actionHandler, so it only controls strafing
-    if input.getBooleanActionValue('Run') then
-        gameSelf.controls.run = not gameSelf.controls.run
-    end
 
     -- No vertical camera movement, unless the player
     if not self.state.isWielding then
         camera.setPitch(0)
     end
 
-    --- Move the player along with the camera as long as they're not in some self.state.where that's already happening
-    if self.state.yawDelta ~= 0 and not CameraManager.isMoving() and not self.state.isWielding then
-        if CameraManager.isThirdPerson() then
-            gameSelf.controls.yawChange = self.state.yawDelta
-        end
-        self.state.yawDelta = 0
-    end
-
     self.state.yawLastFrame = self.state.yawThisFrame
     self.state.pitchLastFrame = self.state.pitchThisFrame
 end
 
-return function()
+---@param managementStore ManagementStore
+---@return CameraManager
+return function(managementStore)
+    assert(managementStore)
+    GlobalManagement = managementStore
+
     return CameraManager
 end
