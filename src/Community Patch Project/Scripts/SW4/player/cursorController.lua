68e20b4758e2f4cbef4f3466975736fbe9939a94
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-05-19 09:20:46 -0700

    FEAT: Implement an activation banner whose text color indicates whether the highlighted object can be activated

--- src/Community Patch Project/Scripts/SW4/player/cursorController.lua
+++ src/Community Patch Project/Scripts/SW4/player/cursorController.lua
@@ -1,12 +1,19 @@
 local async = require 'openmw.async'
 local camera = require 'openmw.camera'
+local gameSelf = require 'openmw.self'
 local input = require 'openmw.input'
+local nearby = require 'openmw.nearby'
+local types = require 'openmw.types'
 local util = require 'openmw.util'
 local ui = require 'openmw.ui'
 
 local ModInfo = require 'scripts.sw4.modinfo'
 
 local I = require 'openmw.interfaces'
+
+local DeltaMultStart = 50
+local DeltaMultEnd = 10000
+local RealActivationRange = 500
 
 ---@type ManagementStore
 local GlobalManagement
@@ -31,12 +38,15 @@ end
 CursorController.state = {
     cursorPos = CursorController:startPos(),
     changeThisFrame = util.vector2(0, 0),
+    shouldShow = false,
+    currentTarget = nil,
     cumulativeXMove = 0,
     flickTriggered = false,
 }
 
 local Cursor = ui.create {
     layer = 'HUD',
+    name = 'SW4_Cursor',
     type = ui.TYPE.Image,
     props = {
         size = util.vector2(CursorController.CursorSize, CursorController.CursorSize),
@@ -47,12 +57,69 @@ local Cursor = ui.create {
     }
 }
 
+local Constants = require 'scripts.omw.mwui.constants'
+local DarkFactor = 0.8
+local LightFactor = 1.25
+
+local DarkColor = util.color.rgb(
+    Constants.normalColor.r * DarkFactor,
+    Constants.normalColor.g * DarkFactor,
+    Constants.normalColor.b * DarkFactor
+)
+
+local LightColor = util.color.rgb(
+    Constants.normalColor.r * LightFactor,
+    Constants.normalColor.g * LightFactor,
+    Constants.normalColor.b * LightFactor
+)
+
+local BannerSize = ui.screenSize():emul(util.vector2(0.15, 0.065))
+local CursorBanner = ui.create {
+    layer = 'HUD',
+    name = 'SW4_CursorBanner',
+    template = I.MWUI.templates.boxTransparent,
+    props = {
+        relativePosition = util.vector2(0.5, 0),
+        anchor = util.vector2(0.5, 0),
+        visible = false,
+    },
+    content = ui.content {
+        {
+            name = 'SW4_CursorBannerText',
+            template = I.MWUI.textHeader,
+            type = ui.TYPE.Text,
+            props = {
+                autoSize = false,
+                size = BannerSize,
+                text = '',
+                textColor = Constants.normalColor,
+                textSize = 24,
+                textAlignH = ui.ALIGNMENT.Center,
+                textAlignV = ui.ALIGNMENT.Center,
+                wordWrap = true,
+                multiline = true,
+            }
+        },
+    }
+}
+
 function CursorController.getCursorIcon(baseName)
     return ('textures/sw4/cursor/%s.dds'):format(baseName)
 end
 
 function CursorController:getCursor()
     return Cursor
+end
+
+function CursorController:getCursorBanner()
+    return CursorBanner
+end
+
+function CursorController:shouldShowCursor()
+    return self.state.shouldShow
+        and not GlobalManagement.LockOn.getMarkerVisibility()
+        and not I.UI.getMode()
+        and camera.getMode() ~= camera.MODE.FirstPerson
 end
 
 function CursorController:onFrameBegin(dt)
@@ -82,7 +149,7 @@ function CursorController:onFrameBegin(dt)
         self.state.flickTriggered = false
     end
 
-    local showCursor = input.getBooleanActionValue('Run') and not markerVisible
+    local showCursor = self:shouldShowCursor()
 
     camera.showCrosshair(not showCursor)
     I.Controls.overrideCombatControls(showCursor)
@@ -94,13 +161,55 @@ function CursorController:onFrameBegin(dt)
     self:setCursorVisible(showCursor)
 end
 
+local canActivate = false
 input.bindAction('Use', async:callback(function()
-        if CursorController:getCursorVisible() then return false end
-        return input.isActionPressed(input.ACTION.Use)
+        local pressed = input.isActionPressed(input.ACTION.Use)
+
+        if CursorController:getCursorVisible() then
+            if not CursorController.state.currentTarget then return false end
+
+            return pressed and canActivate
+        end
+
+        return pressed
     end),
     {})
 
+input.registerActionHandler('Use', async:callback(function(state)
+    if not state or not CursorController:shouldShowCursor() or not CursorController.state.currentTarget then return end
+
+    CursorController.state.currentTarget:activateBy(gameSelf)
+end))
+
+input.registerActionHandler('Run', async:callback(function(state)
+    if not state then return end
+
+    CursorController.state.shouldShow = not CursorController.state.shouldShow
+end))
+
 function CursorController:onFrame(dt)
+    if not self:shouldShowCursor() then return end
+    local rayResult = self:getObjectUnderMouse()
+
+    if not rayResult then return end
+    local scannedObject = rayResult.hitObject
+    local objectName = scannedObject.type.records[scannedObject.recordId].name
+
+    local bannerProps = CursorBanner.layout.props
+    bannerProps.visible = objectName ~= nil
+
+    if objectName then
+        self.state.currentTarget = scannedObject
+
+        local bannerTextProps = CursorBanner.layout.content.SW4_CursorBannerText.props
+        bannerTextProps.textColor = canActivate and LightColor or DarkColor
+        bannerTextProps.text = objectName
+    else
+        self.state.currentTarget = nil
+    end
+
+    CursorBanner:update()
+
     if self.state.changeThisFrame:length() == 0 then return end
 end
 
@@ -152,8 +261,26 @@ function CursorController:getCursorMove()
     return input.getMouseMoveX(), input.getMouseMoveY()
 end
 
-local SelectRange = 1250
+function CursorController.checkTarget(ray)
+    if ray.hitObject and types.Actor.objectIsInstance(ray.hitObject) then return true end
+    local delta = ray.hitPos - gameSelf.position
+    return delta.z < 160 or delta.z < 0.5 * delta:length()
+end
+
 function CursorController:getObjectUnderMouse()
+    local delta = camera.viewportToWorldVector(self:getNormalizedCursorPosition())
+    local basePos = camera.getPosition() + delta * DeltaMultStart
+    local endPos = basePos + delta * DeltaMultEnd
+
+    local options, result = { ignore = { gameSelf, } }, nil
+
+    for _, rayType in pairs { nearby.castRay, nearby.castRenderingRay } do
+        result = rayType(basePos, endPos, options)
+        if result.hitObject and self.checkTarget(result) then
+            canActivate = (basePos - result.hitPos):length() < RealActivationRange
+            return result
+        end
+    end
 end
 
 ---@param newPos util.vector2
