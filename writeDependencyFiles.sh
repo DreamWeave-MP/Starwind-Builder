d71476d580da48483330b24e81ea774723e33f97
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-12-15 11:54:55 -0600

    CLEANUP: Minor improvement to dependency files script

--- writeDependencyFiles.sh
+++ writeDependencyFiles.sh
@@ -10,4 +10,4 @@ mkdir -p $HOME/.config/openmw
 
 echo "data=\"$(pwd)\"\ncontent=Morrowind.esm\ncontent=Tribunal.esm\ncontent=Bloodmoon.esm\ncontent=Starwind.omwaddon" >> $HOME/.config/openmw/openmw.cfg
 
-printf "${MTM_DECRYPT_KEY}" | gpg --force-mdc --no-tty --passphrase-fd 0 --pinentry-mode loopback --decrypt /plugins/DATA.tar.gz.gpg | tar xzvf - -C ${DIR}
+gpg --batch --yes --force-mdc --passphrase "${MTM_DECRYPT_KEY}" --decrypt /plugins/DATA.tar.gz.gpg | tar xzvf - -C ${DIR}
