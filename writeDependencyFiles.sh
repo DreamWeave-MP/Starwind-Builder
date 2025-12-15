7d0b1ba4f53ebefdd2402309169c4c29dd69bf25
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-12-15 04:45:32 -0600

    FIX: Use decryption key as envvar

--- writeDependencyFiles.sh
+++ writeDependencyFiles.sh
@@ -2,12 +2,11 @@
 
 set -euo
 
-KEY="$1"
-DIR="${2:-.}"
+DIR="${1:-.}"
 
 mkdir -p $HOME/.local/share/openmw/data
 mkdir -p $HOME/.config/openmw
 
 echo "data=\"$(pwd)\"\ncontent=Morrowind.esm\ncontent=Tribunal.esm\ncontent=Bloodmoon.esm\ncontent=Starwind.omwaddon" >> $HOME/.config/openmw/openmw.cfg
 
-printf ${KEY} | gpg --batch --passphrase-fd 0 --decrypt /plugins/DATA.tar.gz.gpg | tar xzvf - -C ${DIR}
+printf ${MTM_DECRYPT_KEY} | gpg --batch --passphrase-fd 0 --decrypt /plugins/DATA.tar.gz.gpg | tar xzvf - -C ${DIR}
