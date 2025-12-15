5bcb9d095d6fec6dabd5e5e25fccffff02a196c7
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-12-15 05:17:09 -0600

    FIX: Use stdin for encryption key

--- writeDependencyFiles.sh
+++ writeDependencyFiles.sh
@@ -10,4 +10,4 @@ mkdir -p $HOME/.config/openmw
 
 echo "data=\"$(pwd)\"\ncontent=Morrowind.esm\ncontent=Tribunal.esm\ncontent=Bloodmoon.esm\ncontent=Starwind.omwaddon" >> $HOME/.config/openmw/openmw.cfg
 
-gpg --passphrase ${MTM_DECRYPT_KEY} --decrypt /plugins/DATA.tar.gz.gpg | tar xzvf - -C ${DIR}
+printf ${MTM_DECRYPT_KEY} | gpg --passphrase-fd 0 --decrypt /plugins/DATA.tar.gz.gpg | tar xzvf - -C ${DIR}
