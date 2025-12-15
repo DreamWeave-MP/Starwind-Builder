62b83ec10a419c5d8b75e3d2f4eb59659aee9b2b
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-12-15 11:14:37 -0600

    FIX: Add --force-mdc when decrypting

--- writeDependencyFiles.sh
+++ writeDependencyFiles.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
 set -euo
-export GPG_TTY=$(tty)
 
 DIR="${1:-.}"
 
@@ -10,4 +9,4 @@ mkdir -p $HOME/.config/openmw
 
 echo "data=\"$(pwd)\"\ncontent=Morrowind.esm\ncontent=Tribunal.esm\ncontent=Bloodmoon.esm\ncontent=Starwind.omwaddon" >> $HOME/.config/openmw/openmw.cfg
 
-printf ${MTM_DECRYPT_KEY} | gpg --passphrase-fd 0 --decrypt /plugins/DATA.tar.gz.gpg | tar xzvf - -C ${DIR}
+gpg --force-mdc --no-tty --passphrase ${MTM_DECRYPT_KEY} --decrypt DATA.tar.gz.gpg | tar xzvf - -C ${DIR}
