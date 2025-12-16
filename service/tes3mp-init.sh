3a1bc517230e3514a8d31c4f78803320aabd1f43
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-12-16 01:55:21 -0600

    FIX: Update init script for GitHub build

--- service/tes3mp-init.sh
+++ service/tes3mp-init.sh
@@ -5,16 +5,16 @@ set -eu
 cd $HOME/TES3MP-server/server
 
 VERSION_FILE="$HOME/TES3MP-server/server/.starwind_version"
-LATEST_COMMIT=$(curl -s 'https://gitlab.com/api/v4/projects/modding-openmw%2FStarwind-Builder/repository/branches/master' | grep -o '"commit":{"id":"\w\+"' | cut -d'"' -f6)
+LATEST_COMMIT=$(curl -s https://api.github.com/repos/DreamWeave-MP/Starwind-Builder/commits/HEAD | grep '"sha": *"[^"]*' | head -1 | cut -f 4 -d \")
 
 if [ ! -f "$VERSION_FILE" ] || [ "$(cat $VERSION_FILE)" != "$LATEST_COMMIT" ]; then
     echo "New version detected, updating data..."
 
     # bsdtar provided by libarchive-tools on ubuntu 22.04 and up
     # It's needed here because other archivers don't necessarily allow reading from stdin
-    curl -L 'https://gitlab.com/modding-openmw/Starwind-Builder/-/jobs/artifacts/master/download?job=dump_requiredDataFiles' | bsdtar -xvf- -C data
-    curl -L 'https://gitlab.com/modding-openmw/Starwind-Builder/-/jobs/artifacts/master/download?job=dump_kTools' | bsdtar -xvf- -O | tar -xz -C data/custom
-    curl -L 'https://gitlab.com/modding-openmw/Starwind-Builder/-/jobs/artifacts/master/download?job=dump_merchants' | bsdtar -xvf- -C data/custom
+    curl -L https://github.com/DreamWeave-MP/Starwind-Builder/releases/download/development/requiredDataFiles.json | bsdtar -xvf- -C data
+    curl -L https://github.com/DreamWeave-MP/Starwind-Builder/releases/download/development/kToolsDB.tar.gz | bsdtar -xvf- -O | tar -xz -C data/custom
+    curl -L https://github.com/DreamWeave-MP/Starwind-Builder/releases/download/development/merchantIndexDatabase.json | bsdtar -xvf- -C data/custom
 
     echo "$LATEST_COMMIT" > "$VERSION_FILE"
 else
