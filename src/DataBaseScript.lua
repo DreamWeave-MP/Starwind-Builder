2b4f6693befdba3090a43d4f8073ec1d68bedd63
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-12-16 01:08:08 -0600

    FIX: Correct plugin name in databaseScript

--- src/DataBaseScript.lua
+++ src/DataBaseScript.lua
@@ -71,7 +71,7 @@ MP_Clavicle = 13,
 MP_Tail = 14
 ]]
 local config = {}
-config.files = {"Starwind.omwaddon"}
+config.files = { "Starwind-TSI.omwaddon" }
 
 local DataBaseCellContent = {}
 local DataBaseListCell = {}
@@ -111,108 +111,106 @@ local TopicList = {}
 local DataBaseScript = {}
 
 DataBaseScript.CreateJsonDataBase = function(eventStatus)
-
 	for x, file in pairs(config.files) do
-
 		local records = (espParser.getRecordsByName(file, "CELL"))
 		local dataTypes = {
 			Unique = {
-				{"NAME", "s", "name"}, --cell description
-				{"DATA", {
-					{"i", "flags"},
-					{"i", "gridX"},
-					{"i", "gridY"}
-				}},
-				{"INTV", "i", "water"}, --water height stored in a int (didn't know about this one until I checked the openmw source, no idea why theres 2 of them)
-				{"WHGT", "f", "water"}, --water height stored in a float
-				{"AMBI", {
-					{"i", "ambientColor"},
-					{"i", "sunlightColor"},
-					{"i", "fogColor"},
-					{"f", "fogDensity"}
-				}},
-				{"RGNN", "s", "region"}, --the region name like "Azura's Coast" used for weather and stuff
-				{"NAM5", "i", "mapColor"},
-				{"NAM0", "i", "refNumCounter"} --when you add a new object to the cell in the editor it gets this refNum then this variable is incremented 
+				{ "NAME", "s", "name" }, --cell description
+				{ "DATA", {
+					{ "i", "flags" },
+					{ "i", "gridX" },
+					{ "i", "gridY" }
+				} },
+				{ "INTV", "i", "water" }, --water height stored in a int (didn't know about this one until I checked the openmw source, no idea why theres 2 of them)
+				{ "WHGT", "f", "water" }, --water height stored in a float
+				{ "AMBI", {
+					{ "i", "ambientColor" },
+					{ "i", "sunlightColor" },
+					{ "i", "fogColor" },
+					{ "f", "fogDensity" }
+				} },
+				{ "RGNN", "s", "region" }, --the region name like "Azura's Coast" used for weather and stuff
+				{ "NAM5", "i", "mapColor" },
+				{ "NAM0", "i", "refNumCounter" } --when you add a new object to the cell in the editor it gets this refNum then this variable is incremented
 			},
 			Multi = {
-				{"NAME", "s", "refId"},
-				{"XSCL", "f", "scale"},
-				{"DELE", "i", "deleted"}, --rip my boi
-				{"DNAM", "s", "destCell"}, --the name of the cell the door takes you too
-				{"FLTV", "i", "lockLevel"}, --door lock level
-				{"KNAM", "s", "key"}, --key refId
-				{"TNAM", "s", "trap"}, --trap spell refId
-				{"UNAM", "B", "referenceBlocked"},
-				{"ANAM", "s", "owner"}, --the npc owner or the item
-				{"BNAM", "s", "globalVariable"}, -- Global variable for use in scripts?
-				{"INTV", "i", "charge"}, --current charge?
-				{"NAM9", "i", "goldValue"}, --https://github.com/OpenMW/openmw/blob/dcd381049c3b7f9779c91b2f6b0f1142aff44c4a/components/esm/cellref.cpp#L163
-				{"XSOL", "s", "soul"},
-				{"CNAM", "s", "faction"}, --faction who owns the item
-				{"INDX", "i", "factionRank"}, --what rank you need to be in the faction to pick it up without stealing?
-				{"XCHG", "i", "enchantmentCharge"}, --max charge?
-				{"DODT", {
-					{"f", "posX"},
-					{"f", "posY"},
-					{"f", "posZ"},
-					{"f", "rotX"},
-					{"f", "rotY"},
-					{"f", "rotZ"}
-				}, "doorDest"}, --the position the door takes you too
-				{"DATA", {
-					{"f", "posX"},
-					{"f", "posY"},
-					{"f", "posZ"},
-					{"f", "rotX"},
-					{"f", "rotY"},
-					{"f", "rotZ"}
-				}, "location"} --the position of the object
+				{ "NAME", "s", "refId" },
+				{ "XSCL", "f", "scale" },
+				{ "DELE", "i", "deleted" },     --rip my boi
+				{ "DNAM", "s", "destCell" },    --the name of the cell the door takes you too
+				{ "FLTV", "i", "lockLevel" },   --door lock level
+				{ "KNAM", "s", "key" },         --key refId
+				{ "TNAM", "s", "trap" },        --trap spell refId
+				{ "UNAM", "B", "referenceBlocked" },
+				{ "ANAM", "s", "owner" },       --the npc owner or the item
+				{ "BNAM", "s", "globalVariable" }, -- Global variable for use in scripts?
+				{ "INTV", "i", "charge" },      --current charge?
+				{ "NAM9", "i", "goldValue" },   --https://github.com/OpenMW/openmw/blob/dcd381049c3b7f9779c91b2f6b0f1142aff44c4a/components/esm/cellref.cpp#L163
+				{ "XSOL", "s", "soul" },
+				{ "CNAM", "s", "faction" },     --faction who owns the item
+				{ "INDX", "i", "factionRank" }, --what rank you need to be in the faction to pick it up without stealing?
+				{ "XCHG", "i", "enchantmentCharge" }, --max charge?
+				{ "DODT", {
+					{ "f", "posX" },
+					{ "f", "posY" },
+					{ "f", "posZ" },
+					{ "f", "rotX" },
+					{ "f", "rotY" },
+					{ "f", "rotZ" }
+				}, "doorDest" }, --the position the door takes you too
+				{ "DATA", {
+					{ "f", "posX" },
+					{ "f", "posY" },
+					{ "f", "posZ" },
+					{ "f", "rotX" },
+					{ "f", "rotY" },
+					{ "f", "rotZ" }
+				}, "location" } --the position of the object
 			}
 		}
-		
+
 		for _, record in pairs(records) do
 			local cell = {}
-			
+
 			for _, dType in pairs(dataTypes.Unique) do
 				local tempData = record:getSubRecordsByName(dType[1])[1]
 				if tempData ~= nil then
 					if type(dType[2]) == "table" then
-						local stream = espParser.Stream:create( tempData.data )
+						local stream = espParser.Stream:create(tempData.data)
 						for _, ddType in pairs(dType[2]) do
-							cell[ ddType[2] ] = struct.unpack( ddType[1], stream:read(4) )
+							cell[ddType[2]] = struct.unpack(ddType[1], stream:read(4))
 						end
 					else
-						cell[ dType[3] ] = struct.unpack( dType[2], tempData.data )
+						cell[dType[3]] = struct.unpack(dType[2], tempData.data)
 					end
 				end
-			end			
+			end
 			cell.objects = {}
 			local currentIndex = nil
 			for _, subrecord in pairs(record.subRecords) do
 				if subrecord.name == "FRMR" then
-					currentIndex = struct.unpack( "i", subrecord.data ).."-0"
+					currentIndex = struct.unpack("i", subrecord.data) .. "-0"
 					cell.objects[currentIndex] = {}
 					cell.objects[currentIndex].refNum = currentIndex
 					cell.objects[currentIndex].scale = 1 --just a default
 				end
-					
+
 				for _, dType in pairs(dataTypes.Multi) do
 					if subrecord.name == dType[1] and currentIndex ~= nil then --if its a subrecord in dataTypes.Multi
-						if type(dType[2]) == "table" then --there are several values in this data
-							local stream = espParser.Stream:create( subrecord.data )
-							for _, ddType in pairs(dType[2]) do --go thrue every value that we want out of this data
-								if dType[3] ~= nil then --store the values in a table
-									if cell.objects[currentIndex][ dType[3] ] == nil then
-										cell.objects[currentIndex][ dType[3] ] = {}
+						if type(dType[2]) == "table" then                   --there are several values in this data
+							local stream = espParser.Stream:create(subrecord.data)
+							for _, ddType in pairs(dType[2]) do               --go thrue every value that we want out of this data
+								if dType[3] ~= nil then                         --store the values in a table
+									if cell.objects[currentIndex][dType[3]] == nil then
+										cell.objects[currentIndex][dType[3]] = {}
 									end
-									cell.objects[currentIndex][ dType[3] ][ ddType[2] ] = struct.unpack( ddType[1], stream:read(4) )
+									cell.objects[currentIndex][dType[3]][ddType[2]] = struct.unpack(ddType[1], stream:read(4))
 								else --store the values directly in the cell
-									cell.objects[currentIndex][ ddType[2] ] = struct.unpack( ddType[1], lenTable[ ddType[1] ] )
+									cell.objects[currentIndex][ddType[2]] = struct.unpack(ddType[1], lenTable[ddType[1]])
 								end
 							end
 						else -- theres only one value in the data
-							cell.objects[currentIndex][ dType[3] ] = struct.unpack( dType[2], subrecord.data )
+							cell.objects[currentIndex][dType[3]] = struct.unpack(dType[2], subrecord.data)
 						end
 					end
 				end
@@ -221,12 +219,11 @@ DataBaseScript.CreateJsonDataBase = function(eventStatus)
 			local checkString = string.find(cell.name, ":")
 			if checkString then
 				jsonCellName = string.gsub(cell.name, ":", ";")
-			end	
+			end
 			tes3mp.LogMessage(enumerations.log.ERROR, jsonCellName)
-			DataBaseListCell[jsonCellName] = true	
+			DataBaseListCell[jsonCellName] = true
 			if not DataBaseCellContent[jsonCellName] then
 				DataBaseCellContent[jsonCellName] = cell.objects
-
 			else
 				if cell.objects then
 					for index, slot in pairs(cell.objects) do
@@ -235,8 +232,8 @@ DataBaseScript.CreateJsonDataBase = function(eventStatus)
 				end
 			end
 
-			jsonInterface.save("custom/Starwind/Cell/"..jsonCellName..".json", DataBaseCellContent[jsonCellName])
-		end	
+			jsonInterface.save("custom/Starwind/Cell/" .. jsonCellName .. ".json", DataBaseCellContent[jsonCellName])
+		end
 		jsonInterface.save("custom/Starwind/CellName.json", DataBaseListCell)
 	end
 	tes3mp.StopServer(0)
@@ -772,15 +769,15 @@ end
 						tableNpc.skills.security = struct.unpack( "B", string.sub(subrecord.data, 29, 29+1))
 						tableNpc.skills.sneak = struct.unpack( "B", string.sub(subrecord.data, 30, 30+1))
 						tableNpc.skills.acrobatics = struct.unpack( "B", string.sub(subrecord.data, 31, 31+1))
-						tableNpc.skills.lightarmor = struct.unpack( "B", string.sub(subrecord.data, 32, 32+1)) 
+						tableNpc.skills.lightarmor = struct.unpack( "B", string.sub(subrecord.data, 32, 32+1))
 						tableNpc.skills.shortblade = struct.unpack( "B", string.sub(subrecord.data, 33, 33+1))
 						tableNpc.skills.marksman = struct.unpack( "B", string.sub(subrecord.data, 34, 34+1))
 						tableNpc.skills.mercantile = struct.unpack( "B", string.sub(subrecord.data, 35, 35+1))
 						tableNpc.skills.speechcraft = struct.unpack( "B", string.sub(subrecord.data, 36, 36+1))
 						tableNpc.skills.handtohand = struct.unpack( "B", string.sub(subrecord.data, 37, 37+1))
-						tableNpc.health = struct.unpack( "H", string.sub(subrecord.data, 39, 39+2)) 
+						tableNpc.health = struct.unpack( "H", string.sub(subrecord.data, 39, 39+2))
 						tableNpc.magicka = struct.unpack( "H", string.sub(subrecord.data, 41, 41+2))
-						tableNpc.fatigue = struct.unpack( "H", string.sub(subrecord.data, 43, 43+2)) 
+						tableNpc.fatigue = struct.unpack( "H", string.sub(subrecord.data, 43, 43+2))
 						tableNpc.disposition = struct.unpack( "B", string.sub(subrecord.data, 45, 45+1))
 						tableNpc.reputation = struct.unpack( "B", string.sub(subrecord.data, 46, 46+1))
 						tableNpc.rank = struct.unpack( "B", string.sub(subrecord.data, 47, 47+1))
@@ -1522,7 +1519,7 @@ end
 				}},
 				{"RGNN", "s", "region"}, --the region name like "Azura's Coast" used for weather and stuff
 				{"NAM5", "i", "mapColor"},
-				{"NAM0", "i", "refNumCounter"} --when you add a new object to the cell in the editor it gets this refNum then this variable is incremented 
+				{"NAM0", "i", "refNumCounter"} --when you add a new object to the cell in the editor it gets this refNum then this variable is incremented
 			},
 			Multi = {
 				{"NAME", "s", "refId"},
@@ -1657,9 +1654,9 @@ end
 			end
 
 			local headType			
-			if string.find(id, "hair") then 
+			if string.find(id, "hair") then
 				headType = "hair"
-			elseif string.find(id, "head") then 
+			elseif string.find(id, "head") then
 				headType = "head"
 			end
 			
